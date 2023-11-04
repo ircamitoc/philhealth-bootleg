2dbaee3aed9b98a8ebccfd139c81885b9f77eabf
Isaiah Rain R. Camitoc <81004648+ircamitoc@users.noreply.github.com>
2023-11-05 02:43:28 +0800

    Fixed grid and box layout + added font

--- react-w-ui/src/components/Hero.tsx
+++ react-w-ui/src/components/Hero.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
-
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 interface HeroProps {
   title?: string;
   subTitle?: string;
@@ -11,61 +11,73 @@ interface HeroProps {
   image: string;
 }
 
+const theme = createTheme({
+  typography: {
+    fontFamily: "Poppins, Arial, sans-serif",
+  },
+});
+
 const Hero = (props: HeroProps) => {
   return (
-    <Grid container sm={12} md={6} sx={{ display: "flex" }}>
-      <Box
-        borderRadius={20}
-        sx={{
-          backgroundColor: "rgba(254, 251, 247, 1)",
-          marginX: {
-            md: 8,
-          },
-          paddingY: 6,
-          marginY: 1,
-          paddingX: {
-            md: 6,
-          },
-          display: "flex",
-          flexDirection: "column",
-        }}
-      >
-        <div
-          style={{
-            position: "relative",
-            width: "100%",
-            height: 0,
-            paddingBottom: "75%",
-            marginBottom: "2.5rem",
+    <ThemeProvider theme={theme}>
+      <Grid container sm={12} md={6} sx={{ display: "flex", padding: {md: 0.5, xs: 5} }}>
+        <Box
+          sx={{
+            backgroundColor: "rgba(254, 251, 247, 1)",
+            marginX: {
+              md: 4,
+            },
+            paddingY: {
+              md: 4,
+              xs: 1
+            },
+            marginY: 1,
+            paddingX: {
+              md: 7,
+              xs: 1
+            },
+            display: "flex",
+            flexDirection: "column",
+            textAlign: "justify"
           }}
         >
-          <img
-            src={props.image}
-            alt={props.title}
+          <div
             style={{
-              maxWidth: "100%",
-              borderRadius: "20px",
-              position: "absolute",
+              position: "relative",
               width: "100%",
-              height: "100%",
+              height: 0,
+              paddingBottom: "75%",
+              marginBottom: "2.5rem",
             }}
-          />
-        </div>
+          >
+            <img
+              src={props.image}
+              alt={props.title}
+              style={{
+                maxWidth: "100%",
+                borderRadius: "20px",
+                position: "absolute",
+                width: "100%",
+                height: "100%",
+              }}
+            />
+          </div>
 
-        <Box>{props.header && <Box>{props.header}</Box>}</Box>
-        <Box>
-          {props.title && <Typography variant="h3">{props.title}</Typography>}
-          {props.subTitle && (
-            <Typography variant="h6">{props.subTitle}</Typography>
-          )}
-          {props.body1 && (
-            <Typography variant="body1" style={{ marginTop: "1rem" }}>
-              {props.body1}
-            </Typography>
-          )}
+          <Box>{props.header && <Box>{props.header}</Box>}</Box>
+          <Box>
+            {props.title && <Typography variant="h3" sx={{ fontWeight: 'bold' }}>{props.title}</Typography>}
+            {props.subTitle && (
+              <Typography variant="h6">{props.subTitle}</Typography>
+            )}
+            {props.body1 && (
+              <Typography variant="body1" style={{ marginTop: "1rem" }}>
+                {props.body1}
+              </Typography>
+            )}
+          </Box>
         </Box>
-      </Box>
-    </Grid>
+      </Grid>
+    </ThemeProvider>
   );
 };
 
