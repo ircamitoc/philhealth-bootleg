3ee42fab5525c7e4b59552b7ad22591f725a6d1f
Isaiah Rain R. Camitoc <81004648+ircamitoc@users.noreply.github.com>
2023-11-06 03:52:02 +0800

    Initialize file

---
+++ react-w-ui/src/components/DisclaimerModal.tsx
@@ -0,0 +1,67 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { useState } from "react";
+import Modal from "@mui/material/Modal";
+import Typography from "@mui/material/Typography";
+
+function DisclaimerModal() {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <a
+        onClick={handleOpen}
+        style={{ cursor: "pointer", textDecoration: "none", color: "inherit" }}
+      >
+        <Typography variant="h5">Disclaimer</Typography>
+      </a>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-title"
+        aria-describedby="modal-description"
+      >
+        <div
+          style={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            backgroundColor: "white",
+            padding: "20px",
+            outline: "none",
+            borderRadius: "20px",
+            boxShadow: "24",
+            border: "10px solid #56BEAA"
+          }}
+        >
+          <Typography variant="h5" id="modal-title">
+            Disclaimer
+          </Typography>
+          <Typography variant="body1" id="modal-description">
+            The PhilHealth Logo, Photographs and information on this Web Site
+            may not be reproduced, copied, or downloaded in any form or by any
+            means -- graphic, electronic, or mechanical, including recording,
+            taping, photocopying, or information storage and retrieval systems
+            -- for use in illegal, damaging or pornographic material.
+            <br /><br />But PhilHealth information may be used for educational or
+            informative purposes and may be reproduced, copied, or downloaded
+            without the written permission from PhilHealth.
+          </Typography>
+          <div>
+            <button onClick={handleClose}>Close</button>
+          </div>
+        </div>
+      </Modal>
+    </div>
+  );
+}
+
+export default DisclaimerModal;
